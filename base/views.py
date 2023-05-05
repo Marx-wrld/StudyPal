7adf7c8c03ecd9e53d390c57986f2febf419fe6a
Marx-wrld <iongonga@kabarak.ac.ke>
2023-05-05 13:08:14 +0300

    Configuring the base app

--- base/views.py
+++ base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request): 
+    return render(request, 'home.html')
+
+def room(request):
+    return render(request, 'room.html')
--- studypal/urls.py
+++ studypal/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include 
+#from django.http import HttpResponse
 
-#Creating Views that is the function based views
+"""Creating Views that is the function based views
 def home(request): #request object, the request object is the http, this is going to tell us what kind of request method is sent, what kind of data is being passed in and what is the user sending to the backend
     return HttpResponse(('Home page'))
+def room(request):
+    return HttpResponse('ROOM')
+#This method would be clumsy since we'd have many urls and path on the urls page and that wy django created apps    
+"""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include('base.urls'))
+    # path('', home),
+    # path('room/', room)
 ]
