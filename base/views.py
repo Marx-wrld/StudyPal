9a89d3e6d5815bfc383af1a70073aae6b5650a9b
Marx-wrld <iongonga@kabarak.ac.ke>
2023-05-10 15:31:32 +0300

    using the pk value to create logic

--- base/views.py
+++ base/views.py
@@ -14,5 +14,12 @@ def home(request):
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)#Passing in a dictionary and specifying the value names
 
-def room(request, pk):
-    return render(request, 'base/room.html')
+def room(request, pk): #pk-primary key
+    #In order to get the pk value, later on we'll use this primary key to query the database but for now we'll use the variable rooms to create some logic
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+
+    return render(request, 'base/room.html', context)
