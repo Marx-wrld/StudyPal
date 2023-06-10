cec9e9a9518f7821a86ddcbc81e533aaaadec41d
Marx-wrld <iongonga@kabarak.ac.ke>
2023-06-10 19:52:29 +0300

    querying the db with pk

--- base/views.py
+++ base/views.py
@@ -12,27 +12,26 @@ rooms = [
     {'id': 4, 'name' : 'Backend Developers'}
 ]
 
-def home(request): 
+def home(request):
+    rooms = Room.objects.all() 
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)#Passing in a dictionary and specifying the value names
 
 def room(request, pk): #pk-primary key
     #In order to get the pk value, later on we'll use this primary key to query the database but for now we'll use the variable rooms to create some logic
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
 
 def createRoom(request): 
     form = RoomForm()
+
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
  
-    context = {'form': form}
+    context = {'form': form} 
     return render(request, 'base/room_form.html', context)
