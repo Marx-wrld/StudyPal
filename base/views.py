3d06e651bf6ad1a610872182a4fccab225614b0f
Marx-wrld <iongonga@kabarak.ac.ke>
2023-06-10 19:33:49 +0300

    updateRoom view

--- base/views.py
+++ base/views.py
@@ -36,3 +36,9 @@ def createRoom(request):
     context = {'form': form} 
     return render(request, 'base/room_form.html', context)
 
+def updateRoom(request, pk):
+    room = Room.objects.get(id= pk)
+    form = RoomForm()
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
