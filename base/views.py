25da4da8389beb904b65242a7287ed751c9a2b69
Marx-wrld <iongonga@kabarak.ac.ke>
2023-06-12 18:39:42 +0300

    Login and register links separation

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
