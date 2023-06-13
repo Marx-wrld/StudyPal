82c2932c363f6e5fd03b42e8f6f4f0a89662d241
Marx-wrld <iongonga@kabarak.ac.ke>
2023-06-13 18:42:17 +0300

    setting username to lowercase and welcome text

--- base/views.py
+++ base/views.py
@@ -27,7 +27,7 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -57,9 +57,14 @@ def registerPage(request):
 
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        if form.is_vaild():
+        if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration!')
             
     return render(request, 'base/login_register.html', {'form': form})
 
