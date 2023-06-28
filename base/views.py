60a4feb2fa1e070c37d4a1eab5bd22d6ea0537df
Marx-wrld <iongonga@kabarak.ac.ke>
2023-06-29 00:08:59 +0300

    Edit profile account completion

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 # Create your views here.
 
@@ -186,4 +186,12 @@ def deleteMessage(request, pk):
 
 @login_required(login_url='login')
 def updateUser(request):
-    return render(request, 'base/update-user.html')
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid:
+            form.save()
+        return redirect('user-profile', pk=user.id)
+    return render(request, 'base/update-user.html', {'form': form})
