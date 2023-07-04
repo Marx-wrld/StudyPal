2a30469a879c823b0adf195b29bc7ff3ecc509f3
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-04 15:34:15 +0300

    class serializers for our rooms

--- base/api/views.py
+++ base/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Room
+from .serializers import RoomSerializer
 
 @api_view(['GET'])
 def getRoutes(request): #view to show us all the routes in our api
@@ -14,5 +15,6 @@ def getRoutes(request): #view to show us all the routes in our api
 @api_view(['GET'])
 def getRooms(request):
     rooms = Room.objects.all()
-    return Response(rooms)
+    serializer = RoomSerializer(rooms, many=True) #many means that their are multiple objects that we are going to serializers
+    return Response(serializer.data)
 #Response cannot return back python objects, it ws able to serialize it in routes because it was a dictionary
