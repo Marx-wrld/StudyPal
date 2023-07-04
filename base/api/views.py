b600721a3f75138cf36287d7638eacc3717c7844
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-04 15:08:18 +0300

    @api_view

--- base/api/views.py
+++ base/api/views.py
@@ -1,9 +1,12 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+@api_view(['GET'])
 def getRoutes(request): #view to show us all the routes in our api
     routes = [ 
         'GET /api',
         'GET /api/rooms', #Allows user to get all the rooms from our route
         'GET /api/rooms/:id' #Allows user to get a single room from our route
     ]
-    return JsonResponse(routes, safe=False) #safe is going to allow this list to be turned into a JSON list data
+    return Response(routes) #[safe=False]safe is going to allow this list to be turned into a JSON list data
