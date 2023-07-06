51a223d4c4fb6b2800b85ac8fc226fa75f08d210
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-06 19:34:43 +0300

    Login user by email final config

--- base/forms.py
+++ base/forms.py
@@ -2,6 +2,11 @@ from django.forms import ModelForm
 from .models import Room, User
 from django.contrib.auth.forms import UserCreationForm
 
+class MyUserCreationForm(UserCreationForm): #creating a new UserCreationForm for our newly added attributes
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password1', 'password2']
+
 class RoomForm(ModelForm):
     class Meta:
         model = Room
--- base/views.py
+++ base/views.py
@@ -3,10 +3,9 @@ from django.db.models import Q
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse 
 from .models import Room, Topic, Message, User
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 # Create your views here.
 
@@ -26,22 +25,22 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
+        email = request.POST.get('email').lower()
         password = request.POST.get('password')
 
         try:
-            user = User.objects.get(username=username) #checking if this user exists if not we want to throw in an error
+            user = User.objects.get(email=email) #checking if this user exists if not we want to throw in an error
         except:
             messages.error(request, 'User does not exist')
 
-        user = authenticate(request, username=username, password=password) #authenticate either throws in an error or throws in a user object that matches these credentials.
+        user = authenticate(request, email=email, password=password) #authenticate either throws in an error or throws in a user object that matches these credentials.
 
         if user is not None:
             login(request, user) 
             return redirect('home')
             #login adds this session into our database and then inside our browser
         else:
-            messages.error(request, 'Username OR Password does not exist')
+            messages.error(request, 'Email OR Password does not exist')
 
     context = {'page': page}
     return render(request, 'base/login_register.html', context)
@@ -52,10 +51,10 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    form = UserCreationForm() 
+    form = MyUserCreationForm() 
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
